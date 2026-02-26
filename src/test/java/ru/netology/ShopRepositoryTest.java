be43cf5546ccd267761a83772175b271a13f6010
Nikolay <mutovin36@gmail.com>
2026-02-26 23:14:57 +0300

    Exception testing 2

--- src/test/java/ru/netology/ShopRepositoryTest.java
+++ src/test/java/ru/netology/ShopRepositoryTest.java
@@ -17,7 +17,7 @@ public class ShopRepositoryTest {
         repo.add(product2);
         repo.add(product3);
 
-        Product[] expected = { product1, product2, product3 };
+        Product[] expected = {product1, product2, product3};
         Product[] actual = repo.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
@@ -30,7 +30,7 @@ public class ShopRepositoryTest {
         repo.add(product3);
         repo.removeById(1);
 
-        Product[] expected = { product2, product3 };
+        Product[] expected = {product2, product3};
         Product[] actual = repo.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
