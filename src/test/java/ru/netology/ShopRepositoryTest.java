a16d9bd77e63b1bb380ea01eb6e977993d391ed3
Nikolay <mutovin36@gmail.com>
2026-02-26 17:28:30 +0300

    Exception testing

---
+++ src/test/java/ru/netology/ShopRepositoryTest.java
@@ -0,0 +1,50 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ShopRepositoryTest {
+    ShopRepository repo = new ShopRepository();
+
+    Product product1 = new Product(1, "Хлеб", 90);
+    Product product2 = new Product(2, "Книга", 799);
+    Product product3 = new Product(3, "Вода", 69);
+
+
+    @Test
+    public void shouldAddProductInRepository() {
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        Product[] expected = { product1, product2, product3 };
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDeleteProductInRepository() {
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+        repo.removeById(1);
+
+        Product[] expected = { product2, product3 };
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test() {
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            repo.removeById(7);
+        });
+    }
+
+}
